2c4751308ad8e60a7574098901e74045460c22dc
Alexander Renev <alex.renyov@gmail.com>
2024-12-01 15:08:05 +0300

    Formatter

--- src/main/scala/solutions/Day01.scala
+++ src/main/scala/solutions/Day01.scala
@@ -7,19 +7,24 @@ class Day01 extends Day {
   override val input = Input(1)
 
   override def solvePart1(input: String): Unit = {
-    val (list1, list2) = parseInput(input).unzip
-    val distance = list1.sorted.zip(list2.sorted).map((v1, v2) => (v1 - v2).abs).sum
+    val (list1, list2) = parseInput(input)
+    val distance =
+      list1.sorted.zip(list2.sorted).map((v1, v2) => (v1 - v2).abs).sum
     println(f"Distance is $distance")
   }
 
   override def solvePart2(input: String): Unit = {
-    val (list1, list2) = parseInput(input).unzip
+    val (list1, list2) = parseInput(input)
     val counts = list2.groupBy(v => v).view.mapValues(_.length)
     val similarity = list1.map(v => v * counts.getOrElse(v, 0)).sum
     println(f"Similarity is $similarity")
   }
 
   private def parseInput(input: String) = {
-    input.split("\n").map(_.split("\\s+").map(_.toInt)).map(ar => (ar(0), ar(1)))
+    input
+      .split("\n")
+      .map(_.split("\\s+").map(_.toInt))
+      .map(ar => (ar(0), ar(1)))
+      .unzip
   }
 }
